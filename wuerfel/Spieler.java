5053b5978bb88c8a28cd5a93657f4a74c5fb8405
GrayMigii <99320018+GrayMigii@users.noreply.github.com>
2022-11-09 17:43:29 +0100

    Add files via upload

---
+++ wuerfel/Spieler.java
@@ -0,0 +1,13 @@
+public class Spieler{
+  //Attribute
+  private String name = "";
+  
+  //Methoden
+  public String getName(){
+    return name;
+    }
+  
+  public void setName(String nameNeu){
+    name = nameNeu;
+    }
+  }
